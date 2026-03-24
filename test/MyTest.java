517fcdef2c19c605364eba84d086914b4581c8a2
tessG <tess@lovegreen.life>
2026-03-24 12:06:24 +0100

    testing

---
+++ test/MyTest.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyTest {
+    Festival festival;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        //ARRANGE
+        festival = new Festival();
+        ArrayList<String> stagesdata = FileIO.readData("data/stages.csv");
+        festival.createStages(stagesdata);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void calcTotalCapcity() {
+        //ACT
+        int actual = festival.calcTotalCapcity();
+        int expected = 7800;
+
+        //ASSERT
+        assertEquals(expected, actual);
+
+    }
+}
